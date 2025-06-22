ead0726bf0f36dcc90177cc29590ba74b6ff570e
Shimi Dubinky <your.github.shimi659d@gmail.com>
2025-06-22 18:21:56 +0300

    feat: Configure Tailwind CSS and cleanup App component

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-900 text-white text-4xl font-bold p-10 text-center">
+      VIP Israel Events - Coming Soon
+    </div>
+  )
+}
+
+export default App
