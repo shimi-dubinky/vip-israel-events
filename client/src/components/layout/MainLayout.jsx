a7291198b1d00815ca96d5c7d6bd67477e92f164
Shimi Dubinky <your.github.shimi659d@gmail.com>
2025-06-30 19:46:53 +0300

    HOTFIX: Correct MainLayout to render nested routes

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,16 +1,19 @@
+import { Outlet } from 'react-router-dom'; // 1. ייבוא קריטי
 import { Header } from './Header';
 import Footer from './Footer';
-import FloatingWhatsApp from './FloatingWhatsApp'; 
+import FloatingWhatsApp from './FloatingWhatsApp';
 
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-primary">
       <Header />
       <main className="flex-grow">
-        {children}
+        {/* 2. החלפת {children} ב-Outlet */}
+        {/* זה אומר לראוטר "כאן תצייר את העמודים הפנימיים" */}
+        <Outlet />
       </main>
       <Footer />
-      <FloatingWhatsApp /> 
+      <FloatingWhatsApp />
     </div>
   );
 };
