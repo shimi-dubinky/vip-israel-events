797200cc9a960ccc6d36eb921341da8c906ecd3c
Shimi Dubinky <your.github.shimi659d@gmail.com>
2025-07-09 13:37:44 +0300

    fix: Define useTranslation hook in HomePage to resolve crash

--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -1,15 +1,18 @@
 import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next'; // <-- 1. הוספת הייבוא החסר
 import { PremiumHero } from "../home/PremiumHero";
 import { ServicesGrid } from "../home/ServicesGrid";
 import { WhyUsSection } from "../home/WhyUsSection";
 import { TestimonialsCarousel } from "../home/TestimonialsCarousel";
 
 const HomePage = () => {
+  const { t } = useTranslation(); // <-- 2. הוספת ההצהרה על הפונקציה t
+
   return (
-     <>
+    <>
       <Helmet>
-         <title>{t('seo_home_title')}</title>
-         <meta name="description" content={t('seo_home_description')} />
+        <title>{t('seo_home_title')}</title>
+        <meta name="description" content={t('seo_home_description')} />
       </Helmet>
       
       <div>
